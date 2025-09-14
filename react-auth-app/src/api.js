44e1f031066a9706530767f7b5010d5fb49f659b
muppudathiganesh <muppuraj11@gmail.com>
2025-09-14 22:42:45 +0530

    Update api.js

--- react-auth-app/src/api.js
+++ react-auth-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_BACKEND_URL || 'https://react-day-19.onrender.com',
+ baseURL: 'http://localhost:8000',
 });
 
 export default api;
